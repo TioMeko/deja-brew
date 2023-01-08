adfae7fa960c6fdf833ea715d4d055bcb5378603
bsimmonds28 <bsimmonds28@gmail.com>
2023-01-08 14:06:05 -0500

    Adding pseudo code

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+/*
+Add event listener to button, save search term, and push to new page
+Get parameters from javascript file #1
+Fetch data from Brewery DB API
+Fetch data from Open Food Facts API
+Select a random fun fact
+Compare current day/time to brewery schedule
+Print Results
+Nearby breweries / open or closed
+Fun Fact
+Save user's recent search results & display on page
+Repeat #1
+*/
