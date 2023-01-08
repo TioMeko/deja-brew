494a6f90b392f12c9704ab87590388fa670223fb
bsimmonds28 <bsimmonds28@gmail.com>
2023-01-08 15:12:11 -0500

    more pseudo-code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,37 @@
 /*
-Add event listener to button, save search term, and push to new page
+CREATE VARIABLES
+ADD COMMENTS ABOVE EACH ADDED SECTION TO EXPLAIN STEPS
+
+
+Step 1 (JS page #1): Get users search term and push to new page
+Add event listener to form, save search term, and push to new page
+>prevent default
+>conditional statement - does not match city then modal that notifies invalid entry
+>push to next page
+>save input to local storage
+
+Step 2 (JS page #2): Capture term and put into the URL
 Get parameters from javascript file #1
+>form.value
+
+Step 3: Fetch
 Fetch data from Brewery DB API
 Fetch data from Open Food Facts API
-Select a random fun fact
+
+Step 4: Print Results
+Nearby breweries
+>Display title, schedule, address, and website
+
+Random fun fact
+>Random number generator and loop over array
+>Modal
+
+Display past searches
+>Pull from local storage
+>Make past searches clickable
+
+
+Cool Additions:
 Compare current day/time to brewery schedule
-Print Results
-Nearby breweries / open or closed
-Fun Fact
-Save user's recent search results & display on page
-Repeat #1
+Suggested cities (dropdown)
 */
