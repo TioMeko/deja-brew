c080366a0d127bd648a941890ad301b33c666b0f
NineTwoProgramming <marasimowicz@ymail.com>
2023-01-17 12:33:24 -0500

    Bug fix and variable renaming

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,10 @@
+var cityNameTwo = document.querySelector('#input-text2');
+var submitBtn2 = document.querySelector('#submit2');
+
 getParam();
+
+// TODO: Make a new fetch function for second page to clear html and repopulate. Might be able
+// to manipulate the URL to use the same fetch.
+
+// TODO: Create the functionality of the favorites button with modal. Maybe add to local
+// storage and delete/input as users like?
