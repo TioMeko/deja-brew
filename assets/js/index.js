5512323a6b526e64648bc809d9a074719dfbc8d9
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-01-17 19:52:50 -0500

    Update index.js
    My Prettier did some formatting changes. (Sorry! I might disable this extension in the future.) The only changes I made were:
    
    1.) Made the phone number text say "N/A" instead of "No number" when there is no number because I did some splicing to the phone number and it looks better this way, although it still says "N/A--" now instead of just "N/A" because of the spliced dashes into the phone number. (I don't know how to fix this.)
    
    2.) I spliced the phone numbers so that it has XXX-XXX-XXXX format.
    
    3.) I added/fixed some Tailwind classes in the cards, including to the map (so that it is styled with rounded edges), to the text (fixed some spacing between the text and the heart button), and  made some of the text yellow.
    
    4. Fixed another z-index issue so that the maps go behind the overlay when you open the favorites modal.

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,20 +27,71 @@ Step 5: Save and display favorites (Katie & Carson)
 */
 
 // Creates states in select dropdown input
-var states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
-var stateName = document.querySelector('#states');
-var submitBtn = document.querySelector('#submit');
-var cityName = document.querySelector('#city');
-var cardContainer = document.querySelector('#card-container');
-var errorModal = document.querySelector('#error-modal');
+var states = [
+  "Alabama",
+  "Alaska",
+  "Arizona",
+  "Arkansas",
+  "California",
+  "Colorado",
+  "Connecticut",
+  "Delaware",
+  "Florida",
+  "Georgia",
+  "Hawaii",
+  "Idaho",
+  "Illinois",
+  "Indiana",
+  "Iowa",
+  "Kansas",
+  "Kentucky",
+  "Louisiana",
+  "Maine",
+  "Maryland",
+  "Massachusetts",
+  "Michigan",
+  "Minnesota",
+  "Mississippi",
+  "Missouri",
+  "Montana",
+  "Nebraska",
+  "Nevada",
+  "New Hampshire",
+  "New Jersey",
+  "New Mexico",
+  "New York",
+  "North Carolina",
+  "North Dakota",
+  "Ohio",
+  "Oklahoma",
+  "Oregon",
+  "Pennsylvania",
+  "Rhode Island",
+  "South Carolina",
+  "South Dakota",
+  "Tennessee",
+  "Texas",
+  "Utah",
+  "Vermont",
+  "Virginia",
+  "Washington",
+  "West Virginia",
+  "Wisconsin",
+  "Wyoming",
+];
+var stateName = document.querySelector("#states");
+var submitBtn = document.querySelector("#submit");
+var cityName = document.querySelector("#city");
+var cardContainer = document.querySelector("#card-container");
+var errorModal = document.querySelector("#error-modal");
 
 var breweryStoredArray = [];
 var breweryFavoritesArray = [];
 
 for (var i = 0; i < states.length; i++) {
-    var option = document.createElement('option')
-    option.textContent = states[i]
-    stateName.append(option);
+  var option = document.createElement("option");
+  option.textContent = states[i];
+  stateName.append(option);
 }
 
 /*
@@ -51,126 +102,147 @@ if(!cityName) {
 
 function newPage(event) {
   event.preventDefault;
-  var queryString = './assets/html/app.html?q=&' + cityName.value.split(' ').join('_') + "&" + stateName.value;
+  var queryString =
+    "./assets/html/app.html?q=&" +
+    cityName.value.split(" ").join("_") +
+    "&" +
+    stateName.value;
   location.assign(queryString);
 }
 
 submitBtn.addEventListener("click", newPage);
 
 function getParam() {
-  var searchParamsArr = document.location.search.split('&');
-  var cityName2 = searchParamsArr[1].split('&').pop();
-  var stateName2 = searchParamsArr[2].split('&').pop();
+  var searchParamsArr = document.location.search.split("&");
+  var cityName2 = searchParamsArr[1].split("&").pop();
+  var stateName2 = searchParamsArr[2].split("&").pop();
 
   fetchRequest(cityName2, stateName2);
 }
 
 function fetchRequest(cityName2, stateName2) {
-    fetch('https://api.openbrewerydb.org/breweries?by_city=' + cityName2 + "&by_state=" + stateName2)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        
-        var isValid = false;
-          for (let i = 0; i < data.length; i++) {
-            if (data[i].city.split(' ').join('_').toUpperCase() == cityName2.toUpperCase()){
-              isValid = true;
-              //brewery latitude
-              var lat = data[i].latitude;
-              //brewery longitude
-              var long = data[i].longitude;
-              //brewery name
-              var breweryName = data[i].name;
-              //brewery street address
-              var address = data[i].street;
-              //brewery city name
-              var cityN = data[i].city;
-              //brewery state
-              var state = data[i].state;
-              //brewery zip code/postal code
-              var zipCode = data[i].postal_code;
-              //brewery phone number
-              var number = data[i].phone;
-              //brewery website url
-              var website = data[i].website_url;
-              
-              //If there is missing information, give value instead of null
-              switch (true) {
-                case lat == null || long == null:
-                  continue;
-                case number == null:
-                  number = "No number";
-                case address == null:
-                  address = "No address";
-                case website == null:
-                  website = "No website";
-              }
-    
-              console.log(lat, long);
-              console.log(breweryName, address, cityN, state, zipCode, number, website);
-              
-            }
-            // saves to object
-            var information = {
-              name: breweryName,
-              street: address,
-              city: cityN,
-              state: state,
-              zipCode: zipCode,
-              phone: number,
-              website: website,
-              lat: lat,
-              long: long
-            }
-            
-            //Add object to array
-            breweryStoredArray.push(information);
-  
+  fetch(
+    "https://api.openbrewerydb.org/breweries?by_city=" +
+      cityName2 +
+      "&by_state=" +
+      stateName2
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var isValid = false;
+      for (let i = 0; i < data.length; i++) {
+        if (
+          data[i].city.split(" ").join("_").toUpperCase() ==
+          cityName2.toUpperCase()
+        ) {
+          isValid = true;
+          //brewery latitude
+          var lat = data[i].latitude;
+          //brewery longitude
+          var long = data[i].longitude;
+          //brewery name
+          var breweryName = data[i].name;
+          //brewery street address
+          var address = data[i].street;
+          //brewery city name
+          var cityN = data[i].city;
+          //brewery state
+          var state = data[i].state;
+          //brewery zip code/postal code
+          var zipCode = data[i].postal_code;
+          //brewery phone number
+          var number = data[i].phone;
+          //brewery website url
+          var website = data[i].website_url;
+
+          //If there is missing information, give value instead of null
+          switch (true) {
+            case lat == null || long == null:
+              continue;
+            case number == null:
+              number = "N/A";
+            case address == null:
+              address = "No address";
+            case website == null:
+              website = "No website";
           }
-          
-          if (!isValid) {
-            console.log("this bitch empty yeet");
-            // errorModal.classList.add("visible");
-            // errorModal.classList.remove("hidden");
-          };
-          
-          console.log(breweryStoredArray);
 
-          //put array into local storage
-          storeBreweries();
+          console.log(lat, long);
+          console.log(
+            breweryName,
+            address,
+            cityN,
+            state,
+            zipCode,
+            number,
+            website
+          );
+        }
+        // saves to object
+        var information = {
+          name: breweryName,
+          street: address,
+          city: cityN,
+          state: state,
+          zipCode: zipCode,
+          phone: number,
+          website: website,
+          lat: lat,
+          long: long,
+        };
 
-        })
-  };
-  
-          //create element dynamically within the loop for each container
-  function storeBreweries() {
-    localStorage.setItem('allBreweries', JSON.stringify(breweryStoredArray));
-    createBreweryHTML();
-  }
-  
-  // TODO: Make a function that will be the html for the Modal window. Can call in fetch so we can use the data
-  function brewCard (brew, i) {
-    return `
+        //Add object to array
+        breweryStoredArray.push(information);
+      }
+
+      if (!isValid) {
+        console.log("this bitch empty yeet");
+        // errorModal.classList.add("visible");
+        // errorModal.classList.remove("hidden");
+      }
+
+      console.log(breweryStoredArray);
+
+      //put array into local storage
+      storeBreweries();
+    });
+}
+
+//create element dynamically within the loop for each container
+function storeBreweries() {
+  localStorage.setItem("allBreweries", JSON.stringify(breweryStoredArray));
+  createBreweryHTML();
+}
+
+// TODO: Make a function that will be the html for the Modal window. Can call in fetch so we can use the data
+function brewCard(brew, i) {
+  return `
       <!-- Card -->
       <div class="p-6 rounded-lg bg-white bg-opacity-75">
-      <div id="map${i}" style="height:180px"></div>
+      <div id="map${i}" style="height:180px" class="z-0 object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl"></div>
         <h2
-          class="mb-8 text-xl font-semibold tracking-widest text-stone-700 uppercase breweryName"
+          class="mb-8 text-xl font-semibold tracking-wide text-yellow-700 uppercase breweryName"
         >
         ${brew.name}
         </h2>
         <p class="mx-auto text-base leading-relaxed text-stone-700">
-        ${brew.street}, ${brew.city}, ${brew.state} ${brew.zipCode}
-        </p>
+          ${brew.street}<br>${brew.city}, ${brew.state} ${brew.zipCode}
+          </p>
+          <p class="mx-auto text-base leading-relaxed text-stone-700">
+          ${brew.phone.slice(0, 3)}-${brew.phone.slice(
+    3,
+    6
+  )}-${brew.phone.slice(6, 11)}
+          </p>
         <p class="mx-auto text-base leading-relaxed text-stone-700">
-        <a class="inline-flex items-center mt-4 font-semibold text-blue-600 lg:mb-0 hover:text-neutral-600" target="_blank" href="${brew.website}">Website »</a>
-        </p>
-        <p class="mx-auto text-base leading-relaxed text-stone-700">
-        ${brew.phone}
-        </p>
+          <a class="inline-flex items-center font-semibold text-yellow-700 lg:mb-0 hover:text-yellow-700" target="_blank" href="${
+            brew.website
+          }">Website »</a>
+          </p>
         <button
-          class="self-center mx-2 my-2 px-3 py-2 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-yellow-600 rounded-xl hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+          class="self-center mt-8 mx-2 my-2 px-3 py-2 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-yellow-600 rounded-xl hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
           type="submit"
         >
           <span
@@ -182,28 +254,35 @@ function fetchRequest(cityName2, stateName2) {
         </button>
       </div>
       <!-- Card End -->
-    `};
-  
-  //Pull information from storage and print to page
-  function createBreweryHTML() {
-    JSON.parse(localStorage.getItem('allBreweries'));
-    console.log(breweryStoredArray[0].name);
+    `;
+}
 
-    for (var i = 0; i < breweryStoredArray.length; i++) {
-      // Create the HTML card content depending on array length
-      var div = document.createElement('div');
-      div.innerHTML = brewCard(breweryStoredArray[i], i);
-      cardContainer.append(div);
+//Pull information from storage and print to page
+function createBreweryHTML() {
+  JSON.parse(localStorage.getItem("allBreweries"));
+  console.log(breweryStoredArray[0].name);
 
-      var map = L.map("map" + i).setView([breweryStoredArray[i].lat, breweryStoredArray[i].long], 15);
-      
-      // Dynamically create mapping through each iteration
-      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        maxZoom: 19,
-        attribution:
-          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      }).addTo(map);
+  for (var i = 0; i < breweryStoredArray.length; i++) {
+    // Create the HTML card content depending on array length
+    var div = document.createElement("div");
+    div.innerHTML = brewCard(breweryStoredArray[i], i);
+    cardContainer.append(div);
 
-      var marker = L.marker([breweryStoredArray[i].lat, breweryStoredArray[i].long]).addTo(map);
-     };
-    };
+    var map = L.map("map" + i).setView(
+      [breweryStoredArray[i].lat, breweryStoredArray[i].long],
+      15
+    );
+
+    // Dynamically create mapping through each iteration
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+
+    var marker = L.marker([
+      breweryStoredArray[i].lat,
+      breweryStoredArray[i].long,
+    ]).addTo(map);
+  }
+}
