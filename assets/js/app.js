d82f0518b70acc835f6f1eee861d6085b3527854
NineTwoProgramming <marasimowicz@ymail.com>
2023-01-18 22:57:49 -0500

    Commented code and cleaned spacing.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,12 @@
 var submitBtn2 = document.querySelector('#submit2');
 
+// Calls getParam from index.js to get parameters from URL
 getParam();
-
 
 // Triggers a new fetch from a search on the second page
 submitBtn2.addEventListener('click', secondPageSearch);
 
+// Calls newPage from index.js to create new query
 function secondPageSearch() {
     newPage();
 };
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,78 +1,70 @@
 // Welcome to Deja Brew!
 
-// Creates states in select dropdown input
-var states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming", ];
+var states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming",];
 var stateName = document.querySelector("#states");
 var submitBtn = document.querySelector("#submit");
 var cityName = document.querySelector("#city");
 var cardContainer = document.querySelector("#card-container");
 var errorModal = document.querySelector("#error-modal");
 
+// Empty array to be used in fetch
 var breweryStoredArray = [];
-var breweryFavoritesArray = [];
 
+// Creates the options for the states drop-down menu
 for (var i = 0; i < states.length; i++) {
   var option = document.createElement("option");
   option.textContent = states[i];
   stateName.append(option);
 };
 
-
+// Creates a queried string to be used in app.html
 function newPage(event) {
   if (event !== undefined) {
     event.preventDefault
   };
-
-  // If working locally, the variable queryString needs to be set to ./assets/html/app.html?q=&
-  var queryString = "/deja-brew/assets/html/app.html?q=&" + cityName.value.split(" ").join("_") + "&" + stateName.value;
+  // If working locally, the variable queryString needs to be set to ./assets/html/app.html?q=& not /deja-brew/assets/html/app.html?q=&
+  var queryString = "./assets/html/app.html?q=&" + cityName.value.split(" ").join("_") + "&" + stateName.value;
   location.href = queryString;
 }
+
+// Checks if there is a button with an id of submit
 if (submitBtn !== null) {
   submitBtn.addEventListener("click", newPage);
 }
 
+// Grabs the city and state parameters from the queried URL and passes them as an arguement to fetchRequest
 function getParam() {
-  var searchParamsArr = document.location.search.split("&");
-  var cityName2 = searchParamsArr[1].split("&").pop();
-  var stateName2 = searchParamsArr[2].split("&").pop();
+  var searchParamArr = document.location.search.split("&");
+  var cityNameParam = searchParamArr[1].split("&").pop();
+  var stateNameParam = searchParamArr[2].split("&").pop();
 
-  fetchRequest(cityName2, stateName2);
+  fetchRequest(cityNameParam, stateNameParam);
 }
 
-function fetchRequest(cityName2, stateName2) {
-  fetch(
-      "https://api.openbrewerydb.org/breweries?by_city=" + cityName2 + "&by_state=" + stateName2)
+// Runs a fetch request to retrieve information from open brew api
+function fetchRequest(cityNameParam, stateNameParam) {
+  fetch("https://api.openbrewerydb.org/breweries?by_city=" + cityNameParam + "&by_state=" + stateNameParam)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       var isValid = false;
       for (let i = 0; i < data.length; i++) {
-        if (
-          data[i].city.split(" ").join("_").toUpperCase() ==
-          cityName2.toUpperCase()
-        ) {
+        if (data[i].city.split(" ").join("_").toUpperCase() == cityNameParam.toUpperCase()) {
+
           isValid = true;
-          //brewery latitude
+
           var lat = data[i].latitude;
-          //brewery longitude
           var long = data[i].longitude;
-          //brewery name
           var breweryName = data[i].name;
-          //brewery street address
           var address = data[i].street;
-          //brewery city name
-          var cityN = data[i].city;
-          //brewery state
+          var city = data[i].city;
           var state = data[i].state;
-          //brewery zip code/postal code
           var zipCode = data[i].postal_code;
-          //brewery phone number
           var number = data[i].phone;
-          //brewery website url
           var website = data[i].website_url;
 
-          //If there is missing information, give value instead of null
+          // If there is missing information, give a value instead of null
           switch (true) {
             case lat == null || long == null:
               continue;
@@ -83,13 +75,12 @@ function fetchRequest(cityName2, stateName2) {
             case website == null:
               website = "No website";
           };
-
         }
-        // saves to object
+        // Create object with information from fetch
         var information = {
           name: breweryName,
           street: address,
-          city: cityN,
+          city: city,
           state: state,
           zipCode: zipCode,
           phone: number,
@@ -98,11 +89,10 @@ function fetchRequest(cityName2, stateName2) {
           long: long,
         };
 
-        //Add object to array
         breweryStoredArray.push(information);
       };
 
-      //Modal message if invalid city name is entered
+      // Modal message if invalid city name is entered or nothing is entered
       if (!isValid) {
         var closeButton = document.querySelector("#close-button");
         errorModal.classList.add("visible");
@@ -118,18 +108,17 @@ function fetchRequest(cityName2, stateName2) {
         });
       };
 
-      //put array into local storage
       storeBreweries();
     });
 }
 
-//create element dynamically within the loop for each container
+// Creates the local storage
 function storeBreweries() {
   localStorage.setItem("allBreweries", JSON.stringify(breweryStoredArray));
   createBreweryHTML();
 }
 
-// This function adds html 
+// creates a template for each card to display the breweries
 function brewCard(brew, i) {
   return `
       <!-- Card -->
@@ -144,7 +133,7 @@ function brewCard(brew, i) {
           ${brew.street}<br>${brew.city}, ${brew.state} ${brew.zipCode}
           </p>
           <p class="mx-auto text-base leading-relaxed text-stone-700">
-          ${brew.phone.slice(0, 3)}-${brew.phone.slice(3,6)}-${brew.phone.slice(6, 11)}
+          ${brew.phone.slice(0, 3)}-${brew.phone.slice(3, 6)}-${brew.phone.slice(6, 11)}
           </p>
         <p class="mx-auto text-base leading-relaxed text-stone-700">
           <a class="inline-flex items-center font-semibold text-yellow-700 lg:mb-0 hover:text-yellow-700" target="_blank" href="${brew.website}">Website »</a>
@@ -165,30 +154,22 @@ function brewCard(brew, i) {
     `;
 };
 
-//Pull information from storage and print to page
+// Pulls data from local storage and creates each card depending on length of array
 function createBreweryHTML() {
   JSON.parse(localStorage.getItem("allBreweries"));
 
-  // Create the HTML card content depending on array length
   for (var i = 0; i < breweryStoredArray.length; i++) {
     var div = document.createElement("div");
     div.innerHTML = brewCard(breweryStoredArray[i], i);
     cardContainer.append(div);
 
-    var map = L.map("map" + i).setView(
-      [breweryStoredArray[i].lat, breweryStoredArray[i].long],
-      15
-    );
-
-    // Dynamically create mapping through each iteration
+    // Dynamically creates mapping through each iteration
+    var map = L.map("map" + i).setView([breweryStoredArray[i].lat, breweryStoredArray[i].long], 15);
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(map);
 
-    var marker = L.marker([
-      breweryStoredArray[i].lat,
-      breweryStoredArray[i].long,
-    ]).addTo(map);
+    var marker = L.marker([breweryStoredArray[i].lat, breweryStoredArray[i].long]).addTo(map);
   };
 };
